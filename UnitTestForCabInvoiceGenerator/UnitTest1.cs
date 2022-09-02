d64105b5ac62997a17a0fd2a3a041f76563de3d6
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-09-02 15:28:21 +0530

    Calculating Total Fare For MultipleRides

--- UnitTestForCabInvoiceGenerator/UnitTest1.cs
+++ UnitTestForCabInvoiceGenerator/UnitTest1.cs
@@ -19,5 +19,17 @@ namespace UnitTestForCabInvoiceGenerator
             double excepted = 43;
             Assert.AreEqual(excepted, fare);
         }
+        [TestMethod]
+        [TestCategory("UC 2")]
+        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORAML);
+            Ride[] rides = { new Ride(2.0, 4), new Ride(3.9, 4) };
+            //calculateing fare for list of rides
+            double summary = invoiceGenerator.CalculateFare(rides);
+            double expectedSummary = 67;
+            Assert.AreEqual(summary, expectedSummary);
+        }
+
     }
 }
