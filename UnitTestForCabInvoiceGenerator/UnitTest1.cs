2b4bfecf76fb36975fbacfe52b80dbfe4490e2f0
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-09-02 15:32:13 +0530

    Comparing Two Object Values are Equal Or Not

--- UnitTestForCabInvoiceGenerator/UnitTest1.cs
+++ UnitTestForCabInvoiceGenerator/UnitTest1.cs
@@ -21,15 +21,36 @@ namespace UnitTestForCabInvoiceGenerator
         }
         [TestMethod]
         [TestCategory("UC 2")]
-        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        public void GivenMultipleRidesShouldReturnTotalFareSummary()
         {
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORAML);
             Ride[] rides = { new Ride(2.0, 4), new Ride(3.9, 4) };
-            //calculateing fare for list of rides
-            double summary = invoiceGenerator.CalculateFare(rides);
-            double expectedSummary = 67;
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 67);
+            Assert.AreEqual(summary.totalFare, expectedSummary.totalFare);
+        }
+        [TestMethod]
+        [TestCategory("UC 3")]
+        public void GivenMultipleRidesShouldReturnInvoiceSummaryUsingOverrideEqualMethod()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORAML);
+            Ride[] rides = { new Ride(2.0, 4), new Ride(3.9, 4) };
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 67);
+            //overiding equals method to check the values in both object are same are not 
+            //because both objects are in diffrent address but they belong to same class
+            var res = summary.Equals(expectedSummary);
+            Assert.IsTrue(res);
+        }
+        [TestMethod]
+        [TestCategory("UC 3")]
+        public void GivenMultipleRidesShouldReturnInvoiceSummaryUsingAreequal()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORAML);
+            Ride[] rides = { new Ride(2.0, 4), new Ride(3.9, 4) };
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 67);
             Assert.AreEqual(summary, expectedSummary);
         }
-
     }
 }
